6120a71fbf0773baf84efce4e4e3e2d56dd0beec
Alexey Sachko <alexey.sachko@gmail.com>
2020-06-12 21:20:45 +0500

    Настроил подключение к базе через .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
 import { VerifyToken } from './users/verify-token.entity';
@@ -10,18 +11,20 @@ import { ServicesModule } from './services/services.module';
 import { Service } from './services/service.entity';
 import { Role } from './users/role.entity';
 
+dotenv.config();
+
 @Module({
   imports: [
     UsersModule,
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'alexey',
-      password: 'example',
-      database: 'onlinesimcard',
+      type: process.env.TYPEORM_CONNECTION as any,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
       entities: [User, VerifyToken, Transaction, Service, Role],
-      synchronize: true,
+      synchronize: !!process.env.TYPEORM_SYNCHRONIZE,
     }),
     AuthModule,
     TransactionsModule,
