1f190448cbb4c9c7e5a54c4d5a79573ab72ab6bf
Alexey Sachko <alexey.sachko@gmail.com>
2020-06-13 11:41:52 +0500

    убрал валидацию пароля

--- src/users/dto/user-signup.dto.ts
+++ src/users/dto/user-signup.dto.ts
@@ -13,8 +13,8 @@ export class UserSignupDto {
   @IsString()
   @MinLength(8)
   @MaxLength(20)
-  @Matches(/^(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).*$/, {
-    message: 'password too weak',
-  })
+  // @Matches(/[^a-zA-Z]/, {
+  //   message: 'password too weak',
+  // })
   password: string;
 }
