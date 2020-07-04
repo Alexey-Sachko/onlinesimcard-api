62f976ee113fabcf9a26af0c6df34c0f17c7dfe7
Alexey Sachko <alexey.sachko@gmail.com>
2020-07-04 12:15:07 +0500

    cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { SwaggerTags } from './swagger/tags';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.enableCors({ origin: ['http://localhost:3000'] });
   app.setGlobalPrefix('api/v1');
   const options = new DocumentBuilder()
