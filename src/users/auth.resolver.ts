8aafdcacb8c19ea7d7da794790bb37c60f302492
Alexey Sachko <alexey.sachko@gmail.com>
2020-07-07 16:26:53 +0500

    добавил тип контекста graphql

--- src/users/auth.resolver.ts
+++ src/users/auth.resolver.ts
@@ -1,15 +1,9 @@
-import {
-  Resolver,
-  Mutation,
-  Args,
-  Context,
-  GraphQLExecutionContext,
-} from '@nestjs/graphql';
+import { Resolver, Mutation, Args, Context } from '@nestjs/graphql';
 import { ValidationPipe } from '@nestjs/common';
-import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { AuthResponseType } from './types/auth-response.type';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { MyGqlContext } from '../../dist/common/my-gql-context.interface';
 
 @Resolver('Auth')
 export class AuthResolver {
@@ -17,13 +11,12 @@ export class AuthResolver {
 
   @Mutation(returns => AuthResponseType)
   async login(
-    @Context() context: GraphQLExecutionContext,
+    @Context() context: MyGqlContext,
     @Args('authCredentialsDto', ValidationPipe)
     authCredentialsDto: AuthCredentialsDto,
   ) {
     const data = await this._authService.login(authCredentialsDto);
-    // @ts-ignore TODO
-    const res: Response = context.res;
+    const res = context.res;
     res.cookie('accessToken', data.accessToken, {
       httpOnly: true,
       maxAge: 1000 * 60 * 15,
@@ -31,4 +24,7 @@ export class AuthResolver {
     });
     return data;
   }
+
+  @Mutation(returns => Boolean)
+  async logout(@Context() context: MyGqlContext) {}
 }
