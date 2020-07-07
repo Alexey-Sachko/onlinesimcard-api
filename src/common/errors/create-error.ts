b8367d9091e8e1b355625ebb44f1ae9df77cf5d2
Alexey Sachko <alexey.sachko@gmail.com>
2020-07-07 09:12:07 +0500

    Обновил логику валидации

--- src/common/errors/create-error.ts
+++ src/common/errors/create-error.ts
@@ -1,14 +1,8 @@
 import { ErrorType } from './error.type';
-import { Constraint } from './constraint.type';
 
-export const createError = (
-  path: string,
-  message: string,
-  constraints?: Constraint[],
-): ErrorType => {
-  const err = new ErrorType();
-  err.path = path;
-  err.message = message;
-  err.constraints = constraints;
-  return err;
+export const createError = (path: string, message: string): ErrorType => {
+  return {
+    path,
+    message,
+  };
 };
--- src/users/user.resolver.ts
+++ src/users/user.resolver.ts
@@ -2,13 +2,13 @@ import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { UserSignupDto } from './dto/user-signup.dto';
 import { RegisterPayloadType } from './types/register-payload.type';
-import { validate } from 'src/common/validation/validate';
+import { validate } from 'src/common/validate';
 
 @Resolver()
 export class UserResolver {
   constructor(private readonly _usersService: UsersService) {}
 
-  @Mutation(returns => RegisterPayloadType, { nullable: true })
+  @Mutation(returns => RegisterPayloadType)
   async register(
     @Args('userSignupDto') userSignupDto: UserSignupDto,
   ): Promise<RegisterPayloadType> {
