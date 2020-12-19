c38abd84fbcd83c94acc15add88e17f95d6cbff2
Alexey Sachko <alexey.sachko@gmail.com>
2020-12-19 21:10:53 +0500

    chore: убрана ошибка из сентри (Unauthorized)

--- src/common/sentry.interceptor.ts
+++ src/common/sentry.interceptor.ts
@@ -3,17 +3,24 @@ import {
   Injectable,
   NestInterceptor,
   CallHandler,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import * as Sentry from '@sentry/minimal';
+
+const ignoreExceptions = [UnauthorizedException];
 
 @Injectable()
 export class SentryInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       tap(null, exception => {
-        Sentry.captureException(exception);
+        if (
+          !ignoreExceptions.some(Exception => exception instanceof Exception)
+        ) {
+          Sentry.captureException(exception);
+        }
       }),
     );
   }
